a1a11845fb540417817075276dba8f2c87cf8156
lloladin123 <blackiealexnader@yahoo.dk>
2024-06-27 15:01:14 +0200

    Got rid of redundant google maps styling

--- src/app/components/GoogleMapsComponent.tsx
+++ src/app/components/GoogleMapsComponent.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { CSSProperties } from "react";
+import React from "react";
 import { GoogleMap, Marker, LoadScript } from "@react-google-maps/api";
 
 const containerStyle = {
@@ -12,24 +12,13 @@ const center = {
   lng: 12.0702486,
 };
 
-const labelStyle: CSSProperties = {
-  textAlign: "center",
-  fontSize: "12px",
-  color: "#1e6873",
-  padding: "5px",
-  position: "relative",
-  top: "-20px", // Adjust top position to move the label up or down
-  left: "-10%", // Adjust left position to move the label left or right
-  transform: "translateX(-50%)", // Center the label horizontally
-};
-
 const googleMapsApiKey: string = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!;
 
 const GoogleMapsComponent = () => {
   return (
     <LoadScript googleMapsApiKey={googleMapsApiKey}>
       <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={13}>
-        <Marker position={center} />
+        <Marker position={center} label="Roskilde Gymnastikforening" />
         <div
           style={{
             position: "absolute",
@@ -37,12 +26,7 @@ const GoogleMapsComponent = () => {
             left: "50%",
             transform: "translate(-50%, -50%)",
           }}
-        >
-          <div className="label  flex flex-col" style={labelStyle}>
-            <p className="text-left">Roskilde</p>
-            <p className="text-left">Gymnastikforening</p>
-          </div>
-        </div>
+        ></div>
       </GoogleMap>
     </LoadScript>
   );
