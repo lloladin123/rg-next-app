d225f62cd924b3b1c8f6df996bf8133587005633
lloladin123 <blackiealexnader@yahoo.dk>
2024-06-27 21:50:29 +0200

    small changes to fixed div

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -11,7 +11,7 @@ const Header = () => {
     <div className="w-full bg-white" style={{ height: "100px" }}>
       <div
         className="fixed w-full bg-white flex justify-between items-center px-4 md:px-8"
-        style={{ height: "90px" }}
+        style={{ height: "80px" }}
       >
         <div
           className="absolute left-4 top-1/2 transform -translate-y-1/2"
