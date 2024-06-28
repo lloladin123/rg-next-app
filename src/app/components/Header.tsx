fb83af12cc161649ae36b0d8693fc4c1c72983f0
lloladin123 <blackiealexnader@yahoo.dk>
2024-06-28 11:36:26 +0200

    added back the scrolling effect with fixed header after cahtgpt removed it for no reason

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -8,28 +8,34 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <div
-      className="fixed zIndex-998 w-full bg-white flex justify-between items-center px-4 md:px-8"
-      style={{ height: "100px", zIndex: "998" }}
-    >
+    <>
       <div
-        className="absolute top-1/2 transform -translate-y-1/2"
-        style={{ zIndex: "999" }}
+        className="fixed zIndex-998 w-full bg-white flex justify-between items-center px-4 md:px-8"
+        style={{ height: "90px", zIndex: "998" }}
       >
-        <Link href="/">
-          <Image
-            src={LogoImage}
-            className="mt-8 rounded-full hover:opacity-75 transition-opacity duration-300"
-            alt="RgLogo"
-            width={110}
-            height={110}
-          />
-        </Link>
+        <div
+          className="absolute top-1/2 transform -translate-y-1/2"
+          style={{ zIndex: "999" }}
+        >
+          <Link href="/">
+            <Image
+              src={LogoImage}
+              className="mt-8 rounded-full hover:opacity-75 transition-opacity duration-300"
+              alt="RgLogo"
+              width={110}
+              height={110}
+            />
+          </Link>
+        </div>
+        <div className={`flex ml-auto`}>
+          <NavBar />
+        </div>
       </div>
-      <div className={`flex ml-auto`}>
-        <NavBar />
-      </div>
-    </div>
+      <div
+        className="w-full"
+        style={{ backgroundColor: "white", height: "100px" }}
+      ></div>
+    </>
   );
 };
 
