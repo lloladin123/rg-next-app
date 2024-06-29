bc3fdd24e065e7cde5c0ff21b9458960d6882cea
lloladin123 <blackiealexnader@yahoo.dk>
2024-06-29 10:13:26 +0200

    added a css border to the bottom of the logo

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -14,18 +14,25 @@ const Header = () => {
         style={{ height: "90px", zIndex: "998" }}
       >
         <div
-          className="absolute top-1/2 transform -translate-y-1/2"
-          style={{ zIndex: "999" }}
+          className="relative mt-8 flex items-center justify-center"
+          style={{ width: "110px", height: "110px", zIndex: "999" }}
         >
-          <Link href="/">
-            <Image
-              src={LogoImage}
-              className="mt-8 rounded-full hover:opacity-75 transition-opacity duration-300"
-              alt="RgLogo"
-              width={110}
-              height={110}
-            />
-          </Link>
+          <div
+            className="absolute top-1/2 transform -translate-y-1/2"
+            style={{ zIndex: "999" }}
+          >
+            <Link href="/">
+              <Image
+                src={LogoImage}
+                className="rounded-full hover:opacity-75 transition-opacity duration-300"
+                alt="RgLogo"
+              />
+            </Link>
+          </div>
+          <div
+            className="rounded-full mt-4 absolute bg-white border-b-2 border-black overflow-hidden"
+            style={{ width: "103px", height: "103px" }}
+          ></div>
         </div>
         <div className={`flex ml-auto`}>
           <NavBar />
