0310a78d58d8794d253c962d97a7b63b9cd87ee9
lloladin123 <blackiealexnader@yahoo.dk>
2024-07-04 20:01:01 +0200

    Added some more style to the Contacts page

--- src/app/Contacts/page.tsx
+++ src/app/Contacts/page.tsx
@@ -8,21 +8,25 @@ const Contacts = () => {
     <div className="flex flex-col w-5/6 mx-auto items-center justify-center">
       {/* Criteria buttons */}
       <div className="flex flex-row space-x-2 space-y-1 flex-wrap items-center justify-center">
-        <button className="p-2 px-16  bg-rg-green rounded-2xl hover:opacity-50 duration-300 ease-in-out">
+        <button className="p-1 px-20 text-sm  bg-rg-green rounded-2xl hover:opacity-50 duration-300 ease-in-out">
           Instruktør
         </button>
-        <button className="p-2 px-16  bg-rg-green rounded-2xl hover:opacity-50 duration-300 ease-in-out">
+        <button className="p-1 px-20 text-sm  bg-rg-green rounded-2xl hover:opacity-50 duration-300 ease-in-out">
           HjælpeInstruktør
         </button>
-        <button className="p-2 px-16  bg-rg-green rounded-2xl hover:opacity-50 duration-300 ease-in-out">
+        <button className="p-1 px-20 text-sm  bg-rg-green rounded-2xl hover:opacity-50 duration-300 ease-in-out">
           Bestyrelse
         </button>
-        <button className="p-2 px-16  bg-rg-green rounded-2xl hover:opacity-50 duration-300 ease-in-out">
+        <button className="p-1 px-20 text-sm  bg-rg-green rounded-2xl hover:opacity-50 duration-300 ease-in-out">
           Suppleanter
         </button>
-        <button className="p-2 px-16  bg-rg-green rounded-2xl hover:opacity-50 duration-300 ease-in-out">
+        <button className="p-1 px-20 text-sm  bg-rg-green rounded-2xl hover:opacity-50 duration-300 ease-in-out">
           Kontaktpersoner
         </button>
+      </div>
+      {/* Instruktør header */}
+      <div className="pt-10 border-t w-full flex items-center justify-center mt-4">
+        <p className="text-3xl">Instruktør</p>
       </div>
       {/* Instructor collection */}
       <div className="flex w-full flex-col space-y-12">
