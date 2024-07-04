a0fff1e41ecca0b4a35b146fb9765f0733ee2a57
lloladin123 <blackiealexnader@yahoo.dk>
2024-07-04 20:16:02 +0200

    Contacts page is now responsive on all devices

--- src/app/Contacts/page.tsx
+++ src/app/Contacts/page.tsx
@@ -5,7 +5,7 @@ import { MdKeyboardArrowDown } from "react-icons/md";
 
 const Contacts = () => {
   return (
-    <div className="flex flex-col w-5/6 mx-auto items-center justify-center">
+    <div className="flex flex-col w-full lg:w-5/6 mx-auto items-center justify-center">
       {/* Criteria buttons */}
       <div className="flex flex-row space-x-2 space-y-1 flex-wrap items-center justify-center">
         <button className="p-1 px-20 text-sm  bg-rg-green rounded-2xl hover:opacity-50 duration-300 ease-in-out">
@@ -31,9 +31,9 @@ const Contacts = () => {
       {/* Instructor collection */}
       <div className="flex w-full flex-col space-y-12">
         {/* Instructor box */}
-        <div className="flex flex-row even:flex-row-reverse w-full items-center justify-center space-x-20 p-4">
+        <div className="flex flex-col md:flex-row md:even:flex-row-reverse w-full items-center justify-center md:space-x-10 p-4">
           {/* Instructor Image */}
-          <div className="relative clip-hexagon h-80 w-2/6">
+          <div className="relative md:odd:ml-8 clip-hexagon h-80 w-80">
             <Image
               alt="Instructor Alexander"
               src={Alexander}
@@ -45,12 +45,12 @@ const Contacts = () => {
           {/*Instructor description */}
           <div className="flex flex-col">
             <p className="text-3xl font-bold">Alexander Thøgersen</p>
-            <p className="">
+            <p className="text-sm">
               Jeg blev inviteret ned og hjælpe af en af mine venner, i sæsonen
               2018/19, på et par hold der
             </p>
             {/* More information button */}
-            <button className="flex flex-row space-x-1 p-1 items-center justify-center flex-nowrap w-2/12 bg-rg-green rounded-2xl hover:opacity-50 duration-300 ease-in-out">
+            <button className="flex flex-row space-x-1 p-1 items-center justify-center flex-nowrap w-24 bg-rg-green rounded-2xl hover:opacity-50 duration-300 ease-in-out">
               <div className="text-sm">Mere info</div>
               <MdKeyboardArrowDown className="bg-white rounded-full text-rg-green scale-90" />
             </button>
