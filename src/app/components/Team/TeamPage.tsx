34fb60a9e4b48286b238a82861e5ba9e8f4e09e6
lloladin123 <blackiealexnader@yahoo.dk>
2024-07-04 09:21:33 +0200

    Aplied comments to the TeamPage

--- src/app/components/Team/TeamPage.tsx
+++ src/app/components/Team/TeamPage.tsx
@@ -37,118 +37,137 @@ const TeamPage: React.FC<TeamPageProps> = ({
   return (
     <div className="w-full md:w-11/12 lg:w-10/12 flex flex-col mx-auto">
       <div className="mt-4">
-        <div className="flex flex-col lg:flex-row">
-          <div className="w-full lg:w-3/6 h-60 lg:h-auto relative overflow-hidden ">
-            <Image
-              className=""
-              alt="Springmusenebillede"
-              src={Springmusene}
-              layout="fill"
-              objectPosition="center"
-              objectFit="cover"
-            />
-          </div>
-          <div className="flex flex-col w-full p-2">
-            <div className="relative flex flex-row space-x-3 items-center">
-              <span className="absolute inset-y-0 left-0 w-1 bg-rg-green z-0"></span>
-              <h1 className="text-xl font-normal">{title}</h1>
+        {/* Team card and description container*/}
+        <div>
+          <div className="flex flex-col lg:flex-row">
+            {/* Image */}
+            <div className="w-full lg:w-3/6 h-60 lg:h-auto relative overflow-hidden ">
+              <Image
+                className=""
+                alt="Springmusenebillede"
+                src={Springmusene}
+                layout="fill"
+                objectPosition="center"
+                objectFit="cover"
+              />
             </div>
-            <div className="flex flex-col lg:flex-row">
-              <div className="flex flex-col p-2 space-y-1 w-full lg:w-4/5 w-full ">
-                <div className="flex flex-row w-full space-x-32">
+            <div className="flex flex-col w-full p-2">
+              {/* Title */}
+              <div className="relative flex flex-row space-x-3 items-center">
+                <span className="absolute inset-y-0 left-0 w-1 bg-rg-green z-0"></span>
+                <h1 className="text-2xl font-normal">{title}</h1>
+              </div>
+              <div className="flex flex-col lg:flex-row mt-2">
+                {/* Left side card information container */}
+                <div className="flex flex-col p-2 space-y-2 w-full lg:w-4/5 w-full ">
+                  {/* Age */}
+                  <div className="flex flex-row w-full space-x-32">
+                    <div className="flex flex-row space-x-1">
+                      <FaRegCalendarDays className="text-rg-green mt-1" />
+                      <p>Alder: </p>
+                    </div>
+                    <p className="font-extralight">{age}</p>
+                  </div>
+                  {/* Price */}
+                  <div className="flex flex-row w-full space-x-32">
+                    <div className="flex flex-row space-x-1">
+                      <FaRegMoneyBillAlt className="text-rg-green mt-1" />
+                      <p>Pris: </p>
+                    </div>
+                    <p className="font-extralight">{price}</p>
+                  </div>
+                  {/* Payment options buttons*/}
                   <div className="flex flex-row space-x-1">
+                    <Link
+                      className="flex flex-row p-1 text-white rounded-2xl bg-rg-green hover:opacity-50  duration-300 ease-in-out"
+                      href="/"
+                    >
+                      <IoIosCheckmark className="mt-1 text-white" /> Samlet
+                      betaling
+                    </Link>
+                    <Link
+                      className="rounded-2xl border p-1 text-black hover:opacity-50 hover:border-green-900 duration-300 ease-in-out"
+                      href="/"
+                    >
+                      Ratebetaling
+                    </Link>
+                    <RxQuestionMarkCircled className="mt-2 text-black rounded-full hover:text-white hover:bg-rg-green duration-300 ease-in-out" />
+                  </div>
+                </div>
+                {/* Right side card information container */}
+                <div className="flex flex-col space-y-2 w-full lg:w-3/5 p-2">
+                  {/* Date to and from */}
+                  <div className="flex flex-row space-x-2">
                     <FaRegCalendarDays className="text-rg-green mt-1" />
-                    <p>Alder: </p>
+                    <p className="font-extralight">{dateFromTo}</p>
                   </div>
-                  <p className="font-extralight">{age}</p>
-                </div>
-                <div className="flex flex-row w-full space-x-32">
-                  <div className="flex flex-row space-x-1">
-                    <FaRegMoneyBillAlt className="text-rg-green mt-1" />
-                    <p>Pris: </p>
+                  {/* Location*/}
+                  <div className="flex flex-row space-x-2">
+                    <FaMapMarkerAlt className="text-rg-green mt-1" />
+                    <div className="flex flex-col">
+                      <p>Kildegårdshallen (Hallen)</p>
+                      <p className="font-extralight">{day}</p>
+                    </div>
                   </div>
-                  <p className="font-extralight">{price}</p>
-                </div>
-                <div className="flex flex-row space-x-1">
-                  <Link
-                    className="flex flex-row p-1 text-white rounded-2xl bg-rg-green hover:opacity-50  duration-300 ease-in-out"
-                    href="/"
-                  >
-                    <IoIosCheckmark className="mt-1 text-white" /> Samlet
-                    betaling
-                  </Link>
-                  <Link
-                    className="rounded-2xl border p-1 text-black hover:opacity-50 hover:border-green-900 duration-300 ease-in-out"
-                    href="/"
-                  >
-                    Ratebetaling
-                  </Link>
-                  <RxQuestionMarkCircled className="mt-2 text-black rounded-full hover:text-white hover:bg-rg-green duration-300 ease-in-out" />
-                </div>
-              </div>
-              <div className="flex flex-col space-y-2 w-full lg:w-3/5 p-2">
-                <div className="flex flex-row space-x-2">
-                  <FaRegCalendarDays className="text-rg-green mt-1" />
-                  <p className="font-extralight">{dateFromTo}</p>
-                </div>
-                <div className="flex flex-row space-x-2">
-                  <FaMapMarkerAlt className="text-rg-green mt-1" />
-                  <div className="flex flex-col">
-                    <p>Kildegårdshallen (Hallen)</p>
-                    <p className="font-extralight">{day}</p>
+                  {/* Instructors*/}
+                  <div className="flex flex-row space-x-2">
+                    <FaUser className="text-rg-green mt-1" />
+                    <div className="flex flex-col">
+                      <p>Instruktører</p>
+                      {instructors.map((instructor, index) => (
+                        <div key={index}>
+                          <p className="font-extralight">
+                            {instructor.isHelper == false
+                              ? instructor.name
+                              : ""}
+                          </p>
+                        </div>
+                      ))}
+                    </div>
                   </div>
-                </div>
-                <div className="flex flex-row space-x-2">
-                  <FaUser className="text-rg-green mt-1" />
-                  <div className="flex flex-col">
-                    <p>Instruktører</p>
-                    {instructors.map((instructor, index) => (
-                      <div key={index}>
-                        <p className="font-extralight">
-                          {instructor.isHelper == false ? instructor.name : ""}
-                        </p>
-                      </div>
-                    ))}
-                  </div>
-                </div>
-                <div className="flex flex-row space-x-2">
-                  <FaUser className="text-rg-green mt-1" />
-                  <div className="flex flex-col">
-                    <p>HjælpeInstruktører</p>
-                    {instructors.map((instructor, index) => (
-                      <div key={index}>
-                        <p className="font-extralight">
-                          {instructor.isHelper ? instructor.name : ""}
-                        </p>
-                      </div>
-                    ))}
+                  {/* Helpers*/}
+                  <div className="flex flex-row space-x-2">
+                    <FaUser className="text-rg-green mt-1" />
+                    <div className="flex flex-col">
+                      <p>HjælpeInstruktører</p>
+                      {instructors.map((instructor, index) => (
+                        <div key={index}>
+                          <p className="font-extralight">
+                            {instructor.isHelper ? instructor.name : ""}
+                          </p>
+                        </div>
+                      ))}
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="flex flex-row space-x-1 p-8">
-          <IoInformationCircleSharp className="text-rg-green mt-1 w-12" />
-          <div className="flex flex-col">
-            <p>Beskrivelse</p>
-            <div className="font-extralight">
-              <p className="flex flex-col space-y-2">
-                {description.map((paragraph, index) => (
-                  <p key={index}>{paragraph}</p>
-                ))}
-              </p>
+          {/* Description */}
+          <div className="flex flex-row space-x-1 p-8">
+            <IoInformationCircleSharp className="text-rg-green mt-1 w-12" />
+            <div className="flex flex-col">
+              <p>Beskrivelse</p>
+              <div className="font-extralight">
+                <p className="flex flex-col space-y-2">
+                  {description.map((paragraph, index) => (
+                    <p key={index}>{paragraph}</p>
+                  ))}
+                </p>
+              </div>
             </div>
           </div>
         </div>
-
+        {/* middlebar with price and apply buttons */}
         <div className="mt-16">
+          {/* Price and border */}
           <div className="border-b py-2">
             <div className="relative flex flex-row space-x-3">
               <span className="absolute inset-y-0 left-0 w-1 bg-rg-green z-0"></span>
               <p className="text-2xl">Pris: {price}</p>
             </div>
           </div>
+          {/* Buttons for Aplying */}
           <div className="flex flex-row mt-4 space-x-1">
             <Link
               className="flex flex-row space-x-1 bg-rg-green p-2 rounded-2xl hover:opacity-50 duration-300 ease-in-out"
@@ -166,6 +185,7 @@ const TeamPage: React.FC<TeamPageProps> = ({
             </Link>
           </div>
         </div>
+        {/* Instructor cards */}
         <div className="flex flex-col mt-16 mb-16">
           <div className="flex flex-col lg:flex-row space-x-0 lg:space-x-5">
             {instructors.map((instructor, index) => (
