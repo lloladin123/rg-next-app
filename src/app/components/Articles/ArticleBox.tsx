fde143e6a9ed377e738fb99b9bbd0fa8a893d87c
lloladin123 <blackiealexnader@yahoo.dk>
2025-06-08 10:02:25 +0200

    Added key to articles.section to fix eslint error on vercel

--- src/app/components/Articles/ArticleBox.tsx
+++ src/app/components/Articles/ArticleBox.tsx
@@ -16,7 +16,7 @@ const ArticleBox: React.FC<ArticleBoxProps> = ({ article }) => {
         <h2>{article.title}</h2>
       </div>
       {article.sections.map((article, index) => (
-        <div className="flex flex-row space-x-16 items-start">
+        <div key={index} className="flex flex-row space-x-16 items-start">
           <p className="w-12 text-center">Stk.{index + 1}.</p>
           <p className="flex-1">{article}</p>
         </div>
